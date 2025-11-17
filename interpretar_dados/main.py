f7f5e99bf35ac55cb9ca7b8f59ab3e8d6ea7443a
Demazel <benjamimhenrique.v@gmail.com>
2025-11-17 12:35:51 -0300

    chore: add initial data processing script and student data CSV

---
+++ interpretar_dados/main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+# import eh para chamar a biblioteca para o sistema
+# nome da biblioteca
+# as bla bla bla - para apelidar a biblioteca
+
+# df quer dizer data frame
+df_alunos = pd.read_csv('alunos.csv')
+# pd.read_csv - faz com que o pandas leia o arquivo csv para o python
+print(df_alunos)
