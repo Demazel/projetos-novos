7caa4f4d72500c16adb3074678f98b0031b2c352
Demazel <benjamimhenrique.v@gmail.com>
2025-11-18 15:27:23 -0300

    fix: update scores data and standard deviation calculation method

--- stdv.py
+++ stdv.py
@@ -1,11 +1,11 @@
 def stdv():
     import pandas as pd
 
-    data = {'scores': [20, 25, 19, 28, 23, 30]}
+    data = {'scores': [2, 3, 5, 8, 10, 9, 8, 82]}
     df = pd.DataFrame(data)
 
     # Calculate standard deviation of the 'scores' column
-    std_dev_scores = df['scores'].std()
+    std_dev_scores = df['scores'].std(ddof=0)
     print(f"Standard deviation of 'scores': {std_dev_scores}")
     
 if __name__ == '__main__':
