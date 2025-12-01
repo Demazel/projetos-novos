a52d4955d3eea1ad22a059600edd967a669acecc
Demazel <benjamimhenrique.v@gmail.com>
2025-11-30 23:31:09 -0300

    feat: add NBA player statistics data and visualization script

---
+++ nba_2025/main.py
@@ -0,0 +1,14 @@
+def main():
+    
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    
+    df_nba_stats = pd.read_csv("/Users/user1/Desktop/projetos-novos/nba_2025/nba_player_stats.csv")
+    
+    df_ppg = df_nba_stats["PPG"]
+    
+    plt.hist(df_ppg)
+    plt.show()
+    
+if __name__ == "__main__":
+    main()
